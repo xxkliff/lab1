920eeab777b3f2e0a7e69e55b9dca07f86fc5717
XXKLIFF <89103949+XXKLIFF@users.noreply.github.com>
2026-09-24 21:10:16 +0300

    style: пофикшена структура файлов

--- src/toolkit/__main__.py
+++ src/toolkit/__main__.py
@@ -1,5 +1,3 @@
-from src.power import power_function
-from src.constants import SAMPLE_CONSTANT
 
 
 def main() -> None:
@@ -8,13 +6,8 @@ def main() -> None:
     :return: Данная функция ничего не возвращает
     """
 
-    target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
+    pass
 
-    result = power_function(target=target, power=degree)
-
-    print(result)
-
-    print(SAMPLE_CONSTANT)
 
 if __name__ == "__main__":
     main()
